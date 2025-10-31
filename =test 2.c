48ddbc71d0011154ec144c53ee9432c51e000d8e
gowthamx123 <munagapatigowtham3@gmail.com>
2025-10-31 09:50:17 +0530

    Add files via upload

---
+++ =test 2.c
@@ -0,0 +1,122 @@
+// Online C++ 
+#include <iostream>
+using namespace std;// Online C++ 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a=10,b=20;
+    cout<<"Addition:"<< a + b<<endl;
+    #include <iostream>
+
+using namespace std;
+int main() {
+    // declare variables
+    string name;
+    int age;
+    float height;
+    double weight;
+char blood_group;    
+    cout <<"enter your name:";
+    cin>>name;
+    
+    cout <<"enter your height(in feet):";
+    cin>>height;
+    
+    cout<<"enter your age:";
+    cin>>age;
+    
+    cout<<"enter your weight(in kgs):";
+    cin>>weight;
+    
+    cout <<"enter your blood group:";
+    cin>>blood_group;
+    
+    return 0;
+}
+    cout<<"substraction:"<<a-b<<endl;
+    //(+,-,*,/,%)
+    return 0;
+}
+
+int main() 
+{
+    int a=10,b=20;
+    cout<<"Addition:"<< a + b<<endl;
+    
+    cout<<"substraction:"<<a-b<<endl;
+    //(+,-,*,/,%)
+    return 0;
+}     
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    
+        cout << " Enter your age:";
+        cin >> a;
+        
+        if (a>=18)
+        {
+            cout <<"eligible to vote";
+        }
+    else
+    {
+         int years_left =18-a;
+     cout<<" you are not eligible to vote" << endl;
+     cout << " you have to wait for"<<years_left <<"years";
+    }
+         return 0;
+}
+         #include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    
+        cout << " Enter your age:";
+        cin >> a;
+        
+        if (a>=18)
+        {
+            cout <<"eligible to vote";
+        }
+    else
+    {
+         int years_left =18-a;
+     cout<<" you are not eligible to vote" << endl;
+     cout << " you have to wait for"<<years_left <<"years";
+    }
+         return 0;
+}
+         #include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    
+        cout << " Enter your age:";
+        cin >> a;
+        
+        if (a>=18)
+        {
+            cout <<"eligible to vote";
+        }
+    else
+    {
+         int years_left =18-a;
+     cout<<" you are not eligible to vote" << endl;
+     cout << " you have to wait for"<<years_left <<"years";
+    }
+         return 0;
+}
+         
